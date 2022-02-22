0be7034ec4ab44193b4d87878241836bcad40420
kevinsequeira2 <kevinsequeira758@gmail.com>
2022-02-22 13:14:30 -0600

    app finalized

---
+++ messages.js
@@ -0,0 +1,10 @@
+const Messages = {
+    fileSaved: 'file saved, you can go to edit ',
+    fileNotSave: 'file not saved, you can`t edit ',
+    requestFileName: '* Name file: ',
+    replaceFile: 'Would you like replace file? (y,n): ',
+    fileNotFound: 'the file not exists ',
+    fileExists: '*** The file already exists *** '
+};
+
+module.exports = Messages;
